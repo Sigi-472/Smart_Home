192c736b4b5b45dfc9d637f830a9129ebb30d942
Familie Menzel <lian.menzel@web.de>
2025-08-09 16:52:47 +0200

    fix

--- Note.py
+++ Note.py
@@ -1,1 +1,27 @@
-from flask import request, jsonify
+from flask import Blueprint, request, jsonify
+from datetime import datetime
+from db_defs import get_engine, get_session, Notiz
+
+note_api = Blueprint('note_api', __name__)  # Blueprint für deine API-Routen
+
+@note_api.route("/note_api/save_note", methods=["POST"])
+def api_save_note():
+  data = request.get_json()
+
+  notiz_text = data.get("notiz")
+  priority = data.get("priorityint")
+  datum_str = data.get("date")
+
+  try:
+    datum_obj = datetime.strptime(datum_str, "%Y-%m-%d").date()
+  except Exception:
+    return jsonify({"error": "Ungültiges Datum"}), 400
+
+  session = get_session(get_engine())
+  neue_notiz = Notiz(Notiz=notiz_text, Priority=priority, Datum=datum_obj)
+
+  session.add(neue_notiz)
+  session.commit()
+  session.close()
+
+  return jsonify({'message': 'Notiz gespeichert'})
--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from datetime import date
 from db_defs import get_engine, create_db, get_session, Notiz
+from Note import *
 
 app = Flask(__name__)
+
+engine = get_engine()         # Datenbank-Engine erstellen (Datei Database.db)
+create_db(engine)             # Tabellen erstellen, falls nicht vorhanden
+session = get_session(engine) #
+app.register_blueprint(note_api)
 
 @app.route('/')
 def home():
